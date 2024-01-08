56940111cf2ac65045474901983c3f46da746a68
drole <genuines.forever@gmail.com>
2024-01-08 17:26:03 +0400

    feat: add alt attribute to new tasks in app.js

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="tasks-list__task-delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="tasks-list__task-icon-delete";
+    deleteButtonImg.alt="";
     deleteButton.appendChild(deleteButtonImg);
 
 
